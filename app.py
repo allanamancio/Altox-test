aa3fd607e05ac5181524fc13be870c3bcaef0a70
allanamancio <allanamancio.rocha@gmail.com>
2018-11-15 21:54:40 -0200

    Testing rdkit functions along with the first version of the test.

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def drawMolecule():
-	return "Hello Molecula!"
+	SMILES = 'CCOCCNSC=O'
+	Img = Draw.MolToImage(Chem.MolFromSmiles(SMILES))
+	Img.show()
+	return "Hello, molecula."
 
 if __name__ == "__main__":
 	app.run(debug=True)
