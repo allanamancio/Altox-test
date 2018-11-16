ac4f61b878b3e5bc8572e186f02566ee43e4b24d
allanamancio <allanamancio.rocha@gmail.com>
2018-11-16 20:08:22 -0200

    First version of the test without the base64 conversion.

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 from rdkit import Chem
 from rdkit.Chem import Draw
 
 app = Flask(__name__)
 
 @app.route("/")
-def drawMolecule():
-	SMILES = 'CCOCCNSC=O'
+def home():
+	return "Hello! Put your SMILES in URL so we can draw your molecule :)."
+
+@app.route("/<SMILES>")
+def drawMolecule(SMILES):
 	Img = Draw.MolToImage(Chem.MolFromSmiles(SMILES))
-	Img.show()
-	return "Hello, molecula."
+	Img.save("static/" + SMILES + ".jpg")
+	return render_template("molecule.html", smiles=SMILES)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=True)	
