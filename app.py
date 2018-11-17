a42125c74ec0447ded98463a304c1dd6d57dbddc
allanamancio <allanamancio.rocha@gmail.com>
2018-11-17 03:42:13 -0200

    Final version of the test.

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template
 from rdkit import Chem
 from rdkit.Chem import Draw
+import base64
+from io import BytesIO
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Hello! Put your SMILES in URL so we can draw your molecule :)."
+	return "Hello! Put your SMILES in URL so we can draw your molecule :). EXAMPLE: localhost:5000/CCOCCNSC=O"
 
 @app.route("/<SMILES>")
 def drawMolecule(SMILES):
+	# Generates molecular image from SMILES
 	Img = Draw.MolToImage(Chem.MolFromSmiles(SMILES))
-	Img.save("static/" + SMILES + ".jpg")
-	return render_template("molecule.html", smiles=SMILES)
+	# Converts generated image to base64 encoding
+	file = BytesIO()
+	Img.save(file, format="JPEG")
+	file.seek(0)
+	Img64 = base64.encodestring(file.read()).decode()
+	# Outputs image to HTML
+	return render_template("molecule.html", image=Img64)
 
 if __name__ == "__main__":
-	app.run(debug=True)	
+	app.run()
